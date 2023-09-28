e2213fb2a45f5ac1a3884b338f1027936dd1b363
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2023-09-28 01:30:23 -0500

    new changes from my scene
    pause and main menu settings

---
+++ Assets/Scripts/Game/TagManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class sceneManager
+{
+    public static string MainLevel  = "Main Level";
+    public static string Instructions = "Instructions";
+}
