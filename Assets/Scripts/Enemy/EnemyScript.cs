1b856560b30c222e22eb93f3d5e21f5365cbef59
Falquior <61766239+Falquior@users.noreply.github.com>
2023-09-27 23:40:09 -0500

    Creación de enemigo
    Se creo el prefab para los enemigos del juego, dichos exploraran el mapa de forma aleatoria, esquivando las paredes, y cuando detecten al jugador lo perseguirán hasta que lo maten o dicho se aleje demasiado.
    Estos elementos están en una escena llamada EnemyScene, y tanto el script como el prefab enemigo son accesibles desde las respectivas carpetas en la sección de Assets.

---
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    [Header("Enemy moving elements")]
+    [SerializeField] LayerMask groundLayer;
+    NavMeshAgent myNavMeshAgent;
+    bool confirmDestino = false;
+    float range = 5;
+    float t = 0.0f;
+    public GameObject boxRay;
+    public GameObject playerTarg;
+    public Vector3 destino;
+    string status = "Search";
+    [Header("Objective components")]
+    public GameObject patrolTarget;
+    public Vector3 poscharTarget = new Vector3(0.0f, 0.0f, 0.0f);
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        Debug.Log(status);
+        //Raycast
+        t += 0.1f;
+        boxRay.transform.Rotate(0.0f, 40 * Mathf.Sin(t * 20), 0.0f, Space.Self);
+        RaycastHit hit;
+        if (Physics.Raycast(boxRay.transform.position, boxRay.transform.forward, out hit))
+        {
+            Debug.DrawRay(boxRay.transform.position, boxRay.transform.forward * hit.distance, Color.yellow);
+            //Cambio de estado al detectar al jugador
+            if (hit.collider is SphereCollider)
+            {
+                status = "Attack";
+            }
+        }
+        else
+        {
+            Debug.DrawRay(boxRay.transform.position, boxRay.transform.forward * 1000, Color.red);
+        }
+        //Maquina de estados
+        myNavMeshAgent.speed = 2.5f;
+        switch (status)
+        {
+            case "Search":
+                GoToDest();
+                break;
+            case "Attack":
+                StartCoroutine(Attack());
+                break;
+        }
+    }
+    IEnumerator Attack()
+    {
+        myNavMeshAgent.SetDestination(playerTarg.transform.position);
+        float distance = Vector3.Distance(transform.position, playerTarg.transform.position);
+        if (distance < 2)
+        {
+            Destroy(playerTarg);
+            status = "Search";
+        }
+        else if (distance > 5)
+        {
+            status = "Search";
+        }
+        yield return null;
+    }
+    void GoToDest()
+    {
+        if (!confirmDestino) SearchForDest();
+        if (confirmDestino) myNavMeshAgent.SetDestination(destino);
+        if (Vector3.Distance(transform.position, destino) < 2) confirmDestino = false;
+    }
+    void SearchForDest()
+    {
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
+        destino = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+        if (Physics.Raycast(destino, Vector3.down, groundLayer))
+        {
+            confirmDestino = true;
+        }
+    }
+}
